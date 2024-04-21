0c12b46e4b5b437fdec5b4c07b212cca6aa266d2
Mario Martins Ramos <mario_martins@id.uff.br>
2024-04-21 13:13:13 -0300

    Update __init__.py

--- wellpathpy/__init__.py
+++ wellpathpy/__init__.py
@@ -1,8 +1,3 @@
-try:
-    import pkg_resources
-    __version__ = pkg_resources.get_distribution(__name__).version
-except pkg_resources.DistributionNotFound:
-    pass
 
 __all__ = [
     'read_header_json',
